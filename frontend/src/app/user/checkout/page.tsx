ae5048492c5563c0c4ce0b4850337c197ccd8ba5
Rohit Kumar <rohitrajyadav6522@gmail.com>
2026-08-31 23:59:53 +0530

    Make Checkout page mobile responsive

--- frontend/src/app/user/checkout/page.tsx
+++ frontend/src/app/user/checkout/page.tsx
@@ -336,7 +336,7 @@ function CheckoutContent() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 py-5 sm:py-8">
         <CheckoutStepper currentStep={2} />
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+<div className="grid grid-cols-1 lg:grid-cols-3 gap-5 sm:gap-6">
           {/* Left: Address + Payment forms */}
           <div className="lg:col-span-2 space-y-6">
             {/* Shipping Address */}
